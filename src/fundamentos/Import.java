052c74c72033349a255e8a3c09e9df120f2c8934
tiagofpr <tiagofpr@outlook.com>
2022-04-03 18:18:39 -0300

    curso basico java udemy

---
+++ src/fundamentos/Import.java
@@ -0,0 +1,17 @@
+package fundamentos;
+
+import javax.swing.*;
+import java.util.Date;
+
+public class Import {
+    public static void main(String[] args) {
+        String s = "Bom dia!";
+        System.out.println(s);
+
+        Date d  = new Date();//preciso importar para estar disponivel para uso.
+        System.out.println(d);
+
+
+        //JButton botao = new JButton();
+    }
+}
